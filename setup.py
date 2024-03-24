fbdb9b00f76c19b6f2714253d0d3cdcc111b763c
Piyush Kumar Prasad <128150786+PiyushPrasad01@users.noreply.github.com>
2024-03-24 08:00:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-Project"
-AUTHOR_USER_NAME = "krishnaik06"
+REPO_NAME = "Deep-Learning-Based-Kidney-Disease-Classification-System"
+AUTHOR_USER_NAME = "PiyushPrasad01"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "piyushias2025@gmail.com"
 
 
 setuptools.setup(
@@ -20,9 +20,9 @@ setuptools.setup(
     description="A small python package for CNN app",
     long_description=long_description,
     long_description_content="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    url=f"https://github.com/{PiyushPrasad01}/{Deep-Learning-Based-Kidney-Disease-Classification-System}",
     project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        "Bug Tracker": f"https://github.com/{PiyushPrasad01}/{Deep-Learning-Based-Kidney-Disease-Classification-System}/issues",
     },
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
